f04312300aa092adad2a533bd424fb11da3481d6
Juan Cruz <jc23729@gmail.com>
2021-02-11 14:11:15 -0500

    part 3
    Figure out how to get data on multiple numbers in single request

--- async-await/part1/app.js
+++ async-await/part1/app.js
@@ -1,9 +1,9 @@
 // Part 1: Number Facts
-// Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API. Details.
+// 1 Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API. Details.
 
-// Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
+// 2 Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
 
-// Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. It’s okay if some of the facts are repeats.
+// 3 Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. It’s okay if some of the facts are repeats.
 
 // (Note: You’ll need to make multiple requests for this.)
 
@@ -24,3 +24,13 @@ async function part2() {
   console.log(data);
 }
 
+//3
+async function part3() {
+  let facts = await Promise.all(
+    Array.from({length: 4}, () => $.getJSON(`${baseURL}/${favNumbers}?json`))
+  );
+  facts.forEach(data => {
+    $('body').append(`<p>${data.text}</p>`);
+  });
+}
+part3();
