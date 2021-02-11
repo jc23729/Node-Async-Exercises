64c9709ecbe6d271ac5783833ba431fda4424738
Juan Cruz <jc23729@gmail.com>
2021-02-11 14:36:55 -0500

    function 2
    Make a request to the deck of cards API to request a single card from a newly shuffled deck

--- async-await/part1/part2/app.js
+++ async-await/part1/part2/app.js
@@ -17,3 +17,13 @@ async function part1() {
     let { suit, value } = data.cards[0];
     console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
   }
+//2
+  async function part2() {
+    let firstCardData = await $.getJSON(`${baseURL}/new/draw/`);
+    let deckId = firstCardData.deck_id;
+    let secondCardData = await $.getJSON(`${baseURL}/${deckId}/draw/`);
+    [firstCardData, secondCardData].forEach(card => {
+      let { suit, value } = card.cards[0];
+      console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+    });
+  }
